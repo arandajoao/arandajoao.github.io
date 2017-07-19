96bb972ec28bdba79d8fd353d33a556d65c9cfd4
Joao Aranda <joao.aranda@kiwano.com.br>
2017-07-19 12:26:40 -0300

    começando com o i42n

---
+++ style/js/locations.js
@@ -0,0 +1,25 @@
+i42n = {
+  "pt-BR": {
+    "title": "Olá! Meu nome é João Aranda",
+    "menuInicio" : "Início",
+    "menuPort"  : "Portfolio",
+    "menuAbout" : "Sobre",
+    "menuContact" : "Contato",
+    "menuRedes" : "Redes sociais",
+    "elsewhere_txt" : "Minhas redes sociais",
+    "myDescription" : "Sou um técnico em informática especializado em desenvolvimento web."
+    "text": "Este é um texto de exemplo do uso da biblioteca i42n."
+
+  },
+  "en-US": {
+    "title": "Olá! Meu nome é João Aranda",
+    "menuInicio" : "Início",
+    "menuPort"  : "Portfolio",
+    "menuAbout" : "Sobre",
+    "menuContact" : "Contato",
+    "menuRedes" : "Redes sociais",
+    "elsewhere_txt" : "Minhas redes sociais",
+    "myDescription" : "Sou um técnico em informática especializado em desenvolvimento web."
+      "text": "This is a sample text to show how to use i42n library"
+  }
+}
